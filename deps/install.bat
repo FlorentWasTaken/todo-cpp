7864dc43868b94c31c47992e8a625a4c616f011d
RBreton1 <remy.breton@epitech.eu>
2024-03-18 21:26:35 +0100

    :heavy_plus_sign: Setup conan & Boost

---
+++ deps/install.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set RED=^[[0;31m
+set GREEN=^[[0;32m
+set DEFAULT=^[[0m
+
+where conan >nul 2>&1
+if %errorlevel% neq 0 (
+    echo !RED!Conan isn't installed.!DEFAULT!
+    exit /b 1
+)
+
+conan install .
+
+if !errorlevel! equ 0 (
+    echo !GREEN!Successfully installed dependencies in the current directory.!DEFAULT!
+) else (
+    echo !RED!Error while installing dependencies.!DEFAULT!
+)
+
+endlocal
