fb2b302ecaee46b07e58a5c98fccc40e1967f633
RBreton1 <remy.breton@epitech.eu>
2024-04-09 21:37:14 +0200

    :sparkles: Add tool function (count char)

--- server/source/Tools/tool-string.cpp
+++ server/source/Tools/tool-string.cpp
@@ -14,6 +14,22 @@ bool isAlphaNum(const std::string &str)
 }
 
 /**
+ * Count occurence of a character in a string.
+ * @param const std::string &str | the string to check
+ * @param const char &character | the character to count
+ * @return unsigned int | occurences of character.
+**/
+unsigned int countChar(const std::string &str, const char &character)
+{
+    unsigned int nb = 0;
+
+    for (const char c : str)
+        if (c == character)
+            nb++;
+    return nb;
+}
+
+/**
  * Create an unique ID as a string.
  * @return string | the generated UUID.
 **/
--- server/source/Tools/tool-string.hpp
+++ server/source/Tools/tool-string.hpp
@@ -4,6 +4,8 @@
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
 
-bool isAlphaNum(const std::string& str);
+bool isAlphaNum(const std::string&);
+
+unsigned int countChar(const std::string&, const char&);
 
 std::string createUUIDString();
